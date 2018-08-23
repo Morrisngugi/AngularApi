8f0068f72912aa7dc95cafc5a923a23d1cd2b72f
Morris <ngugimorry@gmail.com>
2018-08-23 08:04:10 +0300

    Add controllers

---
+++ AngularApi/MyScripts/pController.js
@@ -0,0 +1,27 @@
+﻿app.controller('personInfoController', function ($scope, personInfoService) {
+    //The Gender Object
+    $scope.selectedGender = "Male";
+    $scope.Gender = ["Male", "Female"];
+
+    //The Title Object
+    $scope.selectedTitle = "Mr.";
+    $scope.Title = ["Mr.", "Mrs.", "Miss", "Master"];
+    $scope.IsSameAddres = false;
+    //Function to reset Scope Variables
+    function initialize() {
+        $scope.PersonId = 0;
+        $scope.Title = "";
+        $scope.FirstName = "";
+        $scope.MiddleName = "";
+        $scope.LastName = "";
+        $scope.Gender = "";
+        $scope.BirthDate = "";
+        $scope.Age = 0;
+        $scope.CurrentAddress = "";
+        $scope.PermanentAddress = "";
+        $scope.Occupation = "";
+        $scope.Email = "";
+        $scope.MobileNo = "";
+        $scope.DisablesPermanentAddress = false;
+    }
+})
---
+++ AngularApi/MyScripts/pService.js
@@ -0,0 +1,15 @@
+﻿app.Service("personInfoervice", function ($http) {
+    this.getInfo = function (id) {
+        var req = $http.get('/api/personInformationAPI/' + id);
+        return req;
+    };
+
+    this.getAppInfo = function () {
+        var req = $http.get('/api/personInformationAPI');
+        return req;
+    };
+    this.postInfo = function () {
+        var req = $http.post('/api/personInformationAPI', personInfo);
+        return req;
+    };
+});
